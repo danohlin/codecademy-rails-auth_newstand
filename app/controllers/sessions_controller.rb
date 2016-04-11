d86a9633fb5c81fb56d1ed2ff9c998f1275ef023
codecademy <ccuser@codecademy.com>
2016-04-10 22:24:30 -0400

    initial commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  
+  before_action :require_logged_out, only: [:new, :create]
+  
+  def new
+  end
+  
+  def create
+  @user = User.find_by_email(params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      redirect_to 'login'
+    end 
+  end
+  
+  def destroy
+    session[:user_id] = nil 
+    redirect_to '/' 
+  end
+  
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  
+  before_action :require_logged_out, only: [:new, :create]
+  before_action :require_user, only: [:show, :edit, :update]
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+  
+  def show
+      @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+  
+end
